23bdba8c1bcf8f48a61cf66f8a366c1732f3d175
Gonzalo Di Berardino <gonzadiber@gmail.com>
2018-05-02 12:19:03 -0300

    Primer commit. Se sube proyecto comenzado

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Auth::routes();
+
+
+Route::group(['as'=>'admin.','prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth','admin']], function(){
+	Route::get('/dashboard','DashboardController@index')->name('dashboard');
+	Route::resource('tag','TagController');
+});
+
+Route::group(['as'=>'author.','prefix'=>'author','namespace'=>'Author','middleware'=>['auth','author']], function(){
+	Route::get('/dashboard','DashboardController@index')->name('dashboard');
+});
