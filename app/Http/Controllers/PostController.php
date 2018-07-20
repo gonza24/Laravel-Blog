32f11461dbce188e000314008796e2af86e9935f
Gonzalo Di Berardino <gonzadiber@gmail.com>
2018-07-19 22:15:07 -0300

    Se agrega funcionalidad para mostrar y actualizar la cantidad de vistas del post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class PostController extends Controller
 {
     public function details($slug)
     {
     	$post = Post::where('slug', $slug)->first();
+    	
+    	$blogKey = 'blog_' . $post->id;
+    	
+    	if(!Session::has($blogKey))
+    	{
+    		$post->increment('view_count');
+    		Session::put($blogKey,1);
+    	}
+
     	$randomposts = Post::all()->random(3);
     	return view('post', compact('post', 'randomposts'));
     }
