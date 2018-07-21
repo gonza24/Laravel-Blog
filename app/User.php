3c294ced10ddf6cf6febf175203fd1c67c730416
Gonzalo Di Berardino <gonzadiber@gmail.com>
2018-07-21 17:42:36 -0300

    Se crea migracion con modelo Comment junto con relaciones. Se crean relaciones en modelos Post y User

--- app/User.php
+++ app/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany('App\Post')->withTimestamps();
     }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
